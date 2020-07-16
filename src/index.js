e0dfb1bfdc392a0f21fd81d69bc638fab60f3a67
Igor Barbashov <bis200705@gmail.com>
2020-07-16 22:10:36 +0300

    add eslint rules

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-import "./module";
-import "./scss/index.scss";
-
-console.log('Working!');
-
-async function start() {
-    return await Promise.resolve('async working!');
-}
-
-start().then(console.log);
+import './scss/index.scss';
