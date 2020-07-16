2c6198ccfb2c93abb1e2dd1dc7b5b6f458f96db3
Igor Barbashov <bis200705@gmail.com>
2020-07-16 19:45:00 +0300

    add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -36,4 +36,26 @@ module.exports = {
             filename: "bundle.[hash].css",
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    }
+                }
+            }
+        ],
+    }
 }
