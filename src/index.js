0eff07eb9f7e52583f05c802bebc2cb44f2be6d5
IgorBarbashov <barbashovis@lad24.ru>
2020-09-10 12:24:23 +0300

    add components templates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import {Excel} from '@/components/excel/Excel';
+import {Header} from '@/components/header/Header';
+import {ToolBar} from '@/components/toolbar/ToolBar';
+import {Formula} from '@/components/formula/Formula';
+import {Table} from '@/components/table/Table';
 import './scss/index.scss';
+
+const excel = new Excel('#app', {
+  components: [Header, ToolBar, Formula, Table],
+});
+
+excel.render();
